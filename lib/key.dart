3d92e17bb85b3b568224a306b41bcf7bd9f66f3d
AdrianSzacsko <xszacsko@stuba.sk>
2023-05-06 17:21:03 +0200

    modify key dart file with IP addresses

--- lib/key.dart
+++ lib/key.dart
@@ -1,6 +1,6 @@
 const apiKey = "";
 
 //const urlKey = "http://10.0.2.2:8000/";
-const urlKey = "http://192.168.55.91:8000/";
+const urlKey = "http://192.168.28.91:8000/";
 //const urlKey = "http://192.168.0.124:8000/";
 //const urlKey = "http://192.168.0.168:8000/";
